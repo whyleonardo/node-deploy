8969a0c0932fe538610448f3833e80765f5f831d
whyleonardo <pleasefloyd73@gmail.com>
2023-03-03 17:39:54 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '@prisma/client'
+import fastify from 'fastify'
+import {z} from 'zod'
+
+const app = fastify()
+
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return {users}
+})
+
+app.post('/users', async (req, reply) => {
+  const createUserSchema = z.object({
+    name: z.string(),
+    email: z.string().email()
+  })
+
+  const { name, email } = createUserSchema.parse(req.body)
+  
+  await prisma.user.create({
+    data: {
+      name,
+      email
+    }
+  })
+
+  return reply.status(201)
+})
+
+app.listen({
+  host: '0.0.0.0',
+  port: 3333
+})
+  .then(() => console.log('Server is Running!'))
